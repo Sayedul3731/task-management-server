56d5df541468f29e590dfbea3449eaf34dc3878b
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-12-21 20:41:46 +0600

    new to do task save to database

--- index.js
+++ index.js
@@ -26,13 +26,22 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+    const toDoListsCollection = client.db("taskDB").collection("toDoLists")
+
+    app.post("/toDo", async (req, res) => {
+        const newTask = req.body;
+        console.log(newTask);
+        const result = await toDoListsCollection.insertOne(newTask)
+        res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
